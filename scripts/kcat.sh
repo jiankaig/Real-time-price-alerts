23f7312a1741a3c8bfc9084bc7a12974f6bf1fe6
Jiankai G <jiankaig@gmail.com>
2023-03-11 14:51:18 +0800

    working kcat mounting

--- scripts/kcat.sh
+++ scripts/kcat.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
-echo "running kcat.sh!"
-kafkacat -b kafka:9092 -q -u -X auto.offset.reset=earliest -t price-update-topic | \
-jq --compact-output --unbuffered \
-    '. |
-    {   schema: { type: "struct", optional: false, fields: [
-                { type: "string", optional: true, field:"SYM"},
-                { type :"float", optional: true, field:"Price"},
-                { type :"int64", optional: true, field:"LastUpdateTimeStamp_UNIX"}]},
-        payload: {
-            SYM: .SYM,
-            Price: .Price,
-            LastUpdateTimeStamp_UNIX: .LastUpdateTimeStamp_UNIX
-            }
-    }' | \
-kafkacat -b kafka:9092 -t price-update-topic-schema -P -T -u | jq --unbuffered '.'
+echo "kafkacat -b kafka:29092 -q -u -X auto.offset.reset=earliest -t price-update-topic | \
+  jq --compact-output --unbuffered \
+      '. |
+      {   schema: { type: "struct", optional: false, fields: [
+                  { type: "string", optional: true, field:"SYM"},
+                  { type :"float", optional: true, field:"Price"},
+                  { type :"int64", optional: true, field:"LastUpdateTimeStamp_UNIX"}]},
+          payload: {
+              SYM: .SYM,
+              Price: .Price,
+              LastUpdateTimeStamp_UNIX: .LastUpdateTimeStamp_UNIX
+              }
+      }' | kafkacat -b kafka:29092 -t price-update-topic-schema -P -T -u | jq --unbuffered '.'"
--- scripts/run.sh
+++ scripts/run.sh
@@ -46,63 +46,30 @@ curl -s -d @"scripts/jdbc-sink-updating.json" \
     -H "Content-Type: application/json" \
     -X POST http://localhost:8083/connectors | jq .
 
-docker container restart kafkacat
 ### Dirty way to parse json with schema and payload signatures
 ### bridges between two topics
 ### https://rmoff.net/2020/01/22/kafka-connect-and-schemas/
 # echo "Running slient kcat"
-# kcat -b localhost:9092 -q -u -X auto.offset.reset=earliest -t price-update-topic | \
-# jq --compact-output --unbuffered \
-#     '. |
-#     {   schema: { type: "struct", optional: false, fields: [
-#                 { type: "string", optional: true, field:"SYM"},
-#                 { type :"float", optional: true, field:"Price"},
-#                 { type :"int64", optional: true, field:"LastUpdateTimeStamp_UNIX"}]},
-#         payload: {
-#             SYM: .SYM,
-#             Price: .Price,
-#             LastUpdateTimeStamp_UNIX: .LastUpdateTimeStamp_UNIX
-#             }
-#     }' | \
-# kcat -b localhost:9092 -t price-update-topic-schema -P -T -u | jq --unbuffered '.'
+echo "Running slient kcat"
+docker exec -it `docker ps -q --filter name=kafkacat` \
+  /bin/sh -c "kcat -b kafka:29092 -q -u -X auto.offset.reset=earliest -t price-update-topic" | \
+jq --compact-output --unbuffered \
+    '. |
+    {   schema: { type: "struct", optional: false, fields: [
+                { type: "string", optional: true, field:"SYM"},
+                { type :"float", optional: true, field:"Price"},
+                { type :"int64", optional: true, field:"LastUpdateTimeStamp_UNIX"}]},
+        payload: {
+            SYM: .SYM,
+            Price: .Price,
+            LastUpdateTimeStamp_UNIX: .LastUpdateTimeStamp_UNIX
+            }
+    }' | \
+docker exec -i `docker ps -q --filter name=kafkacat` \
+  /bin/sh -c "kcat -b kafka:29092 -t price-update-topic-schema -P -T -u" | \
+jq --unbuffered '.'
 
 # Ctrl+C to exit.
-
-
-## helper commands to view topics
-# echo "listen to kafka-console-consumer source-topic"
-# docker exec -it broker bash /usr/bin/kafka-console-consumer --bootstrap-server localhost:9092 \
-#  --topic source-topic --property print.key=true --from-beginning
-# echo "listen to kafka-console-consumer price-update-topic"
-# docker exec -it broker bash /usr/bin/kafka-console-consumer --bootstrap-server localhost:9092 \
-#  --topic price-update-topic
-# echo "listen to kafka-avro-console-consumer source-topic"
-# docker exec -it schema-registry /usr/bin/kafka-avro-console-consumer --bootstrap-server kafka:29092 \
-#  --topic source-topic --property schema.registry.url=http://localhost:8081
-
-
-# # testing testTopic
-# echo "create jdbc source connections"
-# curl -s -d @"scripts/jdbc-source-test.json" \
-#     -H "Content-Type: application/json" \
-#     -X POST http://localhost:8083/connectors | jq .
-# curl -s -d @"scripts/jdbc-sink-test.json" \
-#     -H "Content-Type: application/json" \
-#     -X POST http://localhost:8083/connectors | jq .
-# echo "check consumer topic.."
-# # docker exec -it broker bash /usr/bin/kafka-console-consumer --bootstrap-server localhost:9092 \
-# #  --topic testTopic #--property print.key=true --from-beginning
-# docker exec -it broker bash /usr/bin/kafka-console-producer \
-#  --bootstrap-server localhost:9092 --topic testTopic 
-
-#  --property value.schema='{"type":"struct",
-#  "fields":[{"type":"int32","optional":false,"field":"id"},
-#  {"type":"string","optional":true,"field":"name"}],"optional":false}'
-
-#   {"id":2,"name":"henry"}
-#   {"schema":{"type":"struct","fields":[{"type":"int32","optional":false,"field":"id"},{"type":"string","optional":true,"field":"name"}],"optional":false},"payload":{"id":3,"name":"tiger"}}
-#   {"schema":{"type":"struct","fields":[{"type":"string","optional":true,"field":"SYM"},{"type":"float","optional":true,"field":"Price"},{"type":"int64","optional":true,"field":"LastUpdateTimeStamp_UNIX"}],"optional":false},"payload":{"SYM":"DEF","Price":3.4,"LastUpdateTimeStamp_UNIX":1678281999}}
-
 
 
 
